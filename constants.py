2121fb8423ab9d11c1670d3ad45a5a809b3514e5
Ruben Alvarez <ruben.amtz@outlook.com>
2023-01-28 14:22:56 -0700

    changed api key loading logic

--- constants.py
+++ constants.py
@@ -12,9 +12,10 @@ ENCODING = "cl100k_base"  # encoding for text-embedding-ada-002
 encoding = tiktoken.get_encoding(ENCODING)
 separator_len = len(encoding.encode(SEPARATOR))
 
-try:
-    openai.api_key_path = 'keys.txt'
-except FileNotFoundError:
+# try to load api key from file, otherwise use environment variable but do not raise error if not found
+if os.path.exists("openai_api_key.txt"):
+    openai.api_key = open("openai_api_key.txt").read().strip()
+else:
     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 COMPLETIONS_API_PARAMS = {
